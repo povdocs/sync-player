21700b2130a30e9c59b5364b8d2af6e69ba35ee5
Brian Chirls <brian@chirls.com>
2014-09-19 13:13:27 -0400

    Tweak heuristics to be more reliable

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function (window) {
 	var CLOCK_PORT = 5001,
-		EPSILON = -1 / 30,
+		EPSILON = -1 / 15,
 
 		maxOffset = 1 / 30,
 		video = document.getElementById('video'),
@@ -14,13 +14,84 @@
 		remoteClock,
 		durationInMilliseconds,
 		timeout,
-		retries = 0;
+		retries = 0,
 
-	/*
-	This runs whenever either the clock accuracy changes or the video duration changes.
-	*/
+		isBuffered;
 
-	function isBuffered(time) {
+	function updateClockDisplay() {
+		clock.textContent = (new Date(remoteClock.time())).toTimeString();
+		videoTime.textContent = ((remoteClock.time() % durationInMilliseconds) / 1000).toFixed(2);
+		requestAnimationFrame(updateClockDisplay);
+	}
+
+	function checkAgain(delay) {
+		clearTimeout(timeout);
+		timeout = setTimeout(checkSync, delay);
+	}
+
+	function checkSync(evt) {
+		var currentTime,
+			current,
+			currentBuffered,
+			targetBuffered,
+			targetDiff,
+			currentDiff,
+			skip;
+
+		//currentTime is the time we should be at NOW
+		currentTime = (remoteClock.time() % durationInMilliseconds) / 1000;
+
+		//targetTime is the time we're seeking to and want to catch up to later
+		//it's a little bit ahead of where we are so we can take time to buffer
+		targetTime = Math.max(targetTime, currentTime);
+
+		currentDiff = currentTime - video.currentTime;
+
+		current = currentDiff > EPSILON && currentDiff < maxOffset;
+		targetBuffered = isBuffered(targetTime);
+		currentBuffered = isBuffered(currentTime) && isBuffered(currentTime + 2);
+
+		if (currentBuffered && current) {
+			video.play();
+			retries = Math.min(2, retries);
+			checkAgain(2000);
+			return;
+		}
+
+		//we missed our window, so seek ahead and try again
+		if (currentDiff >= EPSILON && video.readyState < 2 || currentDiff > 1) {
+			skip = Math.pow(2, Math.min(4, Math.max(retries, 1)));
+			targetTime = currentTime + skip;
+			video.pause();
+			video.currentTime = targetTime;
+			retries++;
+			maxOffset = Math.max(maxOffset, retries * 0.1);
+			checkAgain(1000);
+			return;
+		}
+
+		//we haven't caught up yet, so give it a little more time to buffer and check in again
+		targetDiff = targetTime - currentTime;
+		checkAgain(targetDiff * 500);
+	}
+
+	function stateUpdate(evt) {
+		if (!video.duration) {
+			console.log('No video duration yet');
+			video.pause();
+			return;
+		}
+
+		console.log('video metadata', video.duration, video.videoWidth, video.videoHeight);
+		durationInMilliseconds = Math.round(video.duration * 1000);
+		if (remoteClock.accuracy() > 100) {
+			return;
+		}
+
+		checkSync(evt || 'clock update');
+	}
+
+	function timeBuffered(time) {
 		var i;
 		if (!video.buffered) {
 			return true;
@@ -37,86 +108,15 @@
 		return false;
 	}
 
-	function updateClockDisplay() {
-		clock.textContent = (new Date(remoteClock.time())).toTimeString();
-		videoTime.textContent = video.currentTime.toFixed(2);
-		requestAnimationFrame(updateClockDisplay);
-	}
-
-	function checkAgain(delay) {
-		clearTimeout(timeout);
-		timeout = setTimeout(checkSync, delay);
-	}
-
-	function checkSync() {
-		var currentTime,
-			current,
-			currentBuffered,
-			targetBuffered,
-			targetDiff,
-			currentDiff,
-
-		//currentTime is the time we should be at NOW
-		currentTime = (remoteClock.time() % durationInMilliseconds) / 1000;
-
-		//targetTime is the time we're seeking to and want to catch up to later
-		//it's a little bit ahead of where we are so we can take time to buffer
-		targetTime = Math.max(targetTime, currentTime);
-
-		currentDiff = currentTime - video.currentTime;
-
-		current = currentDiff > EPSILON && currentDiff < maxOffset;
-		targetBuffered = isBuffered(targetTime) ||
-			(targetTime - video.currentTime < 5) && video.readyState >= 3;
-		currentBuffered = isBuffered(currentTime) ||
-			(currentTime - video.currentTime < 1) && video.readyState >= 3;
-
-		if (currentBuffered && current) {
-			if (video.paused) {
-				console.log('buffered and current, so playing', currentTime, video.currentTime);
-			}
-			video.play();
-			retries = 0;
-			checkAgain(checkSync, 1000);
-			return;
-		}
-		console.log('targetBuffered', targetBuffered);
-		console.log('currentBuffered', currentBuffered);
-		console.log('current', current);
-
-		//we missed our window, so seek ahead and try again
-		if (currentDiff >= EPSILON) {
-			targetTime = currentTime + Math.pow(2, Math.min(4, retries / 2 + 1));
-			video.pause();
-			console.log('missed window, seeking from', video.currentTime, 'to', targetTime, retries, 'retries so far');
-			video.currentTime = targetTime;
-			retries++;
-			maxOffset = Math.max(maxOffset, (retries + 1) * 2 / 30);
-			console.log('maxOffset', maxOffset);
-			checkAgain(1000);
-			return;
-		}
-
-		//we haven't caught up yet, so give it a little more time to buffer and check in again
-		targetDiff = targetTime - currentTime;
-		console.log('waiting to catch up', targetDiff);
-		checkAgain(targetDiff * 500);
-	}
-
-	function stateUpdate() {
-		if (!video.duration) {
-			console.log('No video duration yet');
-			video.pause();
-			return;
-		}
-
-		console.log('video metadata', video.duration, video.videoWidth, video.videoHeight);
-		durationInMilliseconds = Math.round(video.duration * 1000);
-		if (remoteClock.accuracy() > 100) {
-			return;
-		}
-
-		checkSync();
+	/*
+	This runs whenever either the clock accuracy changes or the video duration changes.
+	*/
+	if (!video.buffered || 'mozId' in navigator) {
+		isBuffered = function (time) {
+			return (time - video.currentTime < 5) && video.readyState >= 3 || timeBuffered(time);
+		};
+	} else {
+		isBuffered = timeBuffered;
 	}
 
 	serverUrl = location.protocol + '//' + location.hostname + ':' + CLOCK_PORT + '/time-server';
@@ -124,8 +124,8 @@
 
 	video.muted = true;
 	video.addEventListener('durationchange', stateUpdate, false);
-	video.addEventListener('playing', stateUpdate, false);
-	video.addEventListener('seeked', stateUpdate, false);
+	//video.addEventListener('waiting', stateUpdate, false);
+	//video.addEventListener('seeked', stateUpdate, false);
 	video.addEventListener('volumechange', function () {
 		volume.value = video.volume;
 		if (video.muted) {
